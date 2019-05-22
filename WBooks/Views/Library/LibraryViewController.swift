082d4029ac9f322027a70d86a5ddb8151d7d9500
Salvador Carnelutti <salvador.carnelutti@wolox.com.ar>
2019-05-22 13:16:21 -0300

    App localization works fine

--- WBooks/Views/Library/LibraryViewController.swift
+++ WBooks/Views/Library/LibraryViewController.swift
@@ -8,32 +8,33 @@
 
 import Foundation
 import UIKit
+import WolmoCore
 
 class LibraryViewController: UIViewController {
-    
+
     var booksArray : Array = [["book": "A Little Bird Told me", "author": "Timothy Cross"], ["book" : "When The Doves Dissapeared", "author" : "Sofi Oksanen"],
                               ["book" : "The Best Book in the World", "author" : "Peter Sjernstrom"], ["book" : "Be Creative", "author" : "Tony Alcazar"], ["book" : "Redesign the Web", "author" : "Liliana Castilla"]]
     //UIImage "named" part is thejust the name of the image as a String and without the need of the extension.
     let notificationsButton = UIBarButtonItem(image: UIImage(named: "ic_notifications"), style: .plain, target: self, action: #selector(getter: UIDynamicBehavior.action))
     let searchButton = UIBarButtonItem(image: UIImage(named: "ic_search"), style: .plain, target: self, action: #selector(getter: UIDynamicBehavior.action))
 
-    
+
     ///Carga el .xib asociado a la clase LibraryView y se lo asigna a _view.
     private var _view: LibraryView = LibraryView.loadFromNib()!
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         //No carga nada porque luego a la vista del LibraryViewController le voy a asociar es la vista de LibraryView
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     //Que vista va a mostrar el LibraryViewController
     override func loadView() {
         view = _view
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         //A la Table View de la vista LibraryView hay que asignarla como dataSource y delegate (El envia y recibe informacion)
@@ -55,7 +56,7 @@ extension LibraryViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //.dequeue me devuelve una celda reutilizable (Las celdas en el telefono desaparecen y reaparecen segun como se encuentre la vista de la tabla)
         let cell = tableView.dequeue(cell: LibraryCellView.self)!
@@ -65,5 +66,5 @@ extension LibraryViewController: UITableViewDataSource, UITableViewDelegate {
         cell.lblAuthor.text = dict["author"]
         return cell
     }
-    
+
 }
