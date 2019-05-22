4d2207c789661bf1844dc45d0fcf407fb7c895b7
Salvador Carnelutti <salvador.carnelutti@wolox.com.ar>
2019-05-22 12:00:41 -0300

    Navigation bar implemented

--- WBooks/Views/Library/LibraryViewController.swift
+++ WBooks/Views/Library/LibraryViewController.swift
@@ -10,11 +10,16 @@ import Foundation
 import UIKit
 
 class LibraryViewController: UIViewController {
+    
     var booksArray : Array = [["book": "A Little Bird Told me", "author": "Timothy Cross"], ["book" : "When The Doves Dissapeared", "author" : "Sofi Oksanen"],
                               ["book" : "The Best Book in the World", "author" : "Peter Sjernstrom"], ["book" : "Be Creative", "author" : "Tony Alcazar"], ["book" : "Redesign the Web", "author" : "Liliana Castilla"]]
+    //UIImage "named" part is thejust the name of the image as a String and without the need of the extension.
+    let notificationsButton = UIBarButtonItem(image: UIImage(named: "ic_notifications"), style: .plain, target: self, action: #selector(getter: UIDynamicBehavior.action))
+    let searchButton = UIBarButtonItem(image: UIImage(named: "ic_search"), style: .plain, target: self, action: #selector(getter: UIDynamicBehavior.action))
+
+    
     ///Carga el .xib asociado a la clase LibraryView y se lo asigna a _view.
     private var _view: LibraryView = LibraryView.loadFromNib()!
-    
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         //No carga nada porque luego a la vista del LibraryViewController le voy a asociar es la vista de LibraryView
         super.init(nibName: nil, bundle: nil)
@@ -36,6 +41,11 @@ class LibraryViewController: UIViewController {
         _view.libraryTable.delegate = self
         //Asocia la clase LibraryCellView a la Table View
         _view.libraryTable.register(cell: LibraryCellView.self)
+        /*Las propiedades de Navigation Item son modificadas desde la instancia child del UINavigationController
+         porque cuando NavigationController muestra el child, usa la referncia Navigation Item es del child*/
+        self.navigationItem.leftBarButtonItem = notificationsButton
+        self.navigationItem.rightBarButtonItem = searchButton
+        self.navigationItem.title = "LIBRARY"
     }
 }
 
